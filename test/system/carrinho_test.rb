f11e74cf2810c85f3ecada19353b988c7e236d69
jlucartc <jlucartc@gmail.com>
2022-02-10 10:12:00 -0300

    Refatorando testes

--- test/system/carrinho_test.rb
+++ test/system/carrinho_test.rb
@@ -9,6 +9,24 @@ class CarrinhoTest < ApplicationSystemTestCase
     evaluate_script("localStorage.clear()")
   end
 
+  def adiciona_produto_ao_carrinho
+    visit landing_page_path
+    botao_adicionar = find_button(class: 'botao-adicionar-carrinho', match: :first)
+    produto_id = recupera_id(botao_adicionar[:id])
+    botao_adicionar.click
+    produto_id
+  end
+
+  def recupera_id(string)
+    string.split('-').last
+  end
+
+  def carrinho
+    carrinho = evaluate_script("JSON.parse(localStorage.getItem('carrinho'))")
+    carrinho = [] if carrinho.nil?
+    carrinho
+  end
+
   setup do
     limpar_local_storage
   end
@@ -18,28 +36,29 @@ class CarrinhoTest < ApplicationSystemTestCase
   end
 
   test "adiciona produto ao carrinho" do
-    visit landing_page_path
-    carrinho_inicial = evaluate_script("JSON.parse(localStorage.getItem('carrinho'))")
-    botao = find_button(class: 'botao-adicionar-carrinho', match: :first)
-    botao.click
-    contem_id_produto = evaluate_script("JSON.parse(localStorage.getItem('carrinho')).includes(String(#{botao[:id].split('-').last}))")
-    assert_nil carrinho_inicial
+    produto_id = adiciona_produto_ao_carrinho
+    contem_id_produto = evaluate_script("JSON.parse(localStorage.getItem('carrinho')).includes(String(#{produto_id}))")
     assert contem_id_produto
   end
 
   test "remove produto do carrinho" do
-    visit landing_page_path
-    carrinho_inicial = evaluate_script("JSON.parse(localStorage.getItem('carrinho'))")
-    botao_adicionar = find_button(class: 'botao-adicionar-carrinho', match: :first)
-    botao_adicionar.click
-    contem_id_produto = evaluate_script("JSON.parse(localStorage.getItem('carrinho')).includes(String(#{botao_adicionar[:id].split('-').last}))")
-    botao_adicionar_id = botao_adicionar[:id].split('-').last
+    produto_id = adiciona_produto_ao_carrinho
+    carrinho_inicio = carrinho
     visit carrinho_path
-    botao_remover = find_button(class: 'botao-remover-carrinho', match: :first)
+    botao_remover = find_button(class: 'item-carrinho-controles-remover', match: :first)
     botao_remover.click
-    ainda_contem_id_produto = evaluate_script("JSON.parse(localStorage.getItem('carrinho')).includes(String(#{botao_adicionar_id}))")
-    assert_nil carrinho_inicial
-    assert contem_id_produto
-    refute ainda_contem_id_produto
+    carrinho_fim = carrinho
+    assert carrinho_inicio.include?(produto_id)
+    refute carrinho_fim.include?(produto_id)
   end
+
+  test "deve finalizar compra apenas para usuarios logados" do
+    adiciona_produto_ao_carrinho
+    binding.pry
+    visit carrinho_path
+    botao_finalizar_compra = find_button(class: 'botao-finalizar-compra', match: :first)
+    botao_finalizar_compra.click
+    assert false
+  end
+
 end
